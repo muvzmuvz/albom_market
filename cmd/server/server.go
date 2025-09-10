4f74445168a617810b15ff9ab9b9c0e6a5c9b38b
anon <anon@MacBook-Pro-anon.local>
2025-09-10 15:23:44 +0300

    Добавил корс

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -5,14 +5,28 @@ import (
 	"gin/handlers/authHandlers"
 	"gin/middleware"
 	"gin/utils"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func StartServer() error {
 	router := gin.Default()
-	trustErr := router.SetTrustedProxies([]string{"127.0.0.1", "localhost"})
+
+	trustErr := router.SetTrustedProxies([]string{"127.0.0.1"})
+
 	router.Use(utils.LoggerMiddleware())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true, // если используете cookie для JWT
+		MaxAge:           12 * time.Hour,
+	}))
+
 	router.Static("/uploads", "./uploads")
+
 	albumRoutes := router.Group("/albums")
 	{
 		albumRoutes.GET("", albumHandlers.GetAlbums)
@@ -28,12 +42,13 @@ func StartServer() error {
 		userRoutes.GET("", middleware.AuthMiddleware(), middleware.RequireRole("admin"),
 			authHandlers.GetAllUsersHandler)
 	}
-
+	
 	authRoutes := router.Group("/auth")
 	{
 		authRoutes.POST("/register", authHandlers.RegUser)
 		authRoutes.POST("/login", authHandlers.Login)
 	}
+
 	err := router.Run(":8080")
 	if trustErr != nil {
 		return trustErr
